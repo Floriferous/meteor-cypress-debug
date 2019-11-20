38a973a6d2796c087adf48917ad0c93753e86d86
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-11-20 18:48:27 +0100

    Kinda working react

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,12 +1,38 @@
-import React from 'react';
-import Hello from './Hello.jsx';
-import Info from './Info.jsx';
+import React from "react";
+import SimpleSchema from "simpl-schema";
+import AutoForm from "uniforms-material/AutoForm";
+// import AutoFields from "uniforms-material/AutoFields";
+// import AutoField from "uniforms-material/AutoField";
+import "uniforms-material";
+import SimpleSchema2Bridge from "uniforms-bridge-simple-schema-2";
+
+const schema = new SimpleSchema({
+  list: Array,
+  "list.$": Object,
+  "list.$.title": String,
+  "list.$.number": Number
+});
+
+const bridge = new SimpleSchema2Bridge(schema);
+
+// const CustomAutoField = props => {
+//   return <AutoField {...props}></AutoField>;
+// };
+
+const CustomAutoField = AutoField;
 
 const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
-    <Hello />
-    <Info />
+
+    <AutoForm
+      schema={bridge}
+      // onSubmit={values => {
+      //   console.log("submitting", values);
+      // }}
+    >
+      {/* <AutoFields autoField={CustomAutoField} element="div"></AutoFields> */}
+    </AutoForm>
   </div>
 );
 
