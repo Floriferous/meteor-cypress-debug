fe3c3f9e6f82d66468727f849818509964665117
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-11-20 18:50:18 +0100

    Create simple reproduction

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import SimpleSchema from "simpl-schema";
 import AutoForm from "uniforms-material/AutoForm";
-// import AutoFields from "uniforms-material/AutoFields";
-// import AutoField from "uniforms-material/AutoField";
 import "uniforms-material";
 import SimpleSchema2Bridge from "uniforms-bridge-simple-schema-2";
 
@@ -15,24 +13,13 @@ const schema = new SimpleSchema({
 
 const bridge = new SimpleSchema2Bridge(schema);
 
-// const CustomAutoField = props => {
-//   return <AutoField {...props}></AutoField>;
-// };
-
-const CustomAutoField = AutoField;
+// const CustomAutoField = AutoField;
 
 const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
 
-    <AutoForm
-      schema={bridge}
-      // onSubmit={values => {
-      //   console.log("submitting", values);
-      // }}
-    >
-      {/* <AutoFields autoField={CustomAutoField} element="div"></AutoFields> */}
-    </AutoForm>
+    <AutoForm schema={bridge}></AutoForm>
   </div>
 );
 
